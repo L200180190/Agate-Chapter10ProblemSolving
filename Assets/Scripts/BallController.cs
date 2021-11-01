d949d896d5e72dc8632b2bd95c63e43be1f89869
Ulin Nuha Trisiana <utrisiana@gmail.com>
2021-11-02 01:53:12 +0700

    Add files via upload

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+
+    Vector2 direction;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        
+        direction = Vector2.one.normalized;
+    }
+
+    private void FixedUpdate() {
+        rigidBody2D.velocity = direction * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction.y = -direction.y;
+        }
+        else if (collision.gameObject.CompareTag("SideWall"))
+        {
+            direction.x = -direction.x;
+        }
+    }
+}
